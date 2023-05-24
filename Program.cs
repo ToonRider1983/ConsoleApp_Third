abe478dbd0a5be00d2ef65d20241a8e715f8dd1b
Jiradej Kerdsiri <toonrider2526@gmail.com>
2023-05-24 14:51:03 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Third {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            int quantityOfStudent = 1, studentCounter = 1;
+            int[] midScore, finalScore, sumScore;
+            string[] studentName;
+
+            // Firstly, The program ask the user about student quantity.
+            Console.Write("How much student? : ");
+            quantityOfStudent = int.Parse( Console.ReadLine() );
+
+            // Secondly, re-allocating array sizing.
+            studentName = new string[quantityOfStudent];
+            midScore = new int[quantityOfStudent];
+            finalScore = new int[quantityOfStudent];
+            sumScore = new int[quantityOfStudent];
+
+            // Thirdly, The user feild each student information.
+            Console.WriteLine(":: Student Information ::");
+            for (int iterator = 0; iterator < quantityOfStudent; iterator++)
+            {
+                // User feild student name, middle and final score information.
+                Console.Write($"\nPlease key student({studentCounter} of {quantityOfStudent}) name : ");
+                studentName[iterator] = Console.ReadLine();
+
+                Console.Write($"Please key {studentName[iterator]}'s middle score : ");
+                midScore[iterator] = Convert.ToInt32( Console.ReadLine() );
+
+                Console.Write($"Please key {studentName[iterator]}'s final score : ");
+                finalScore[iterator] = Convert.ToInt32(Console.ReadLine());
+
+                // Score summation 
+                sumScore[iterator] = midScore[iterator] + finalScore[iterator];
+
+                // increment the counter variable values
+                studentCounter++;
+            }
+
+            Console.WriteLine("\n:: Show score student's score and graded ::");
+            int _iterator = 0;
+
+            do
+            {
+                Console.WriteLine($"{studentName[_iterator]} have total score is {sumScore[_iterator]}");
+            } while (_iterator <= quantityOfStudent);
+
+
+            Console.Write("\nPress any key enter....");
+            Console.ReadLine();
+        }
+    }
+}
